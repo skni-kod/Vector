aa3f8e17363e076770b53f16c23cfd1f9b9960aa
Artur Szałyga <arturszalyga@wp.pl>
2022-04-11 23:55:30 +0200

    Init Commit

---
+++ Source/Vector/Vector.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Vector.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Vector, "Vector" );
---
+++ Source/Vector/VectorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VectorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VECTOR_API AVectorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
